012f77ad69dc20a2fa5cfa66201bb13afde4dcb4
thinker227 <thinker0227@gmail.com>
2023-12-02 11:19:18 +0100

    Cleanup

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -30,48 +30,48 @@ public sealed class Day02 : Day
         var possible =
             from x in FlattenCubes(games)
             let cubes = x.cubes
-            let r = Min(cubes[0], 12)
-            let g = Min(cubes[1], 13)
-            let b = Min(cubes[2], 14)
+            let r = Max(cubes[0], 12)
+            let g = Max(cubes[1], 13)
+            let b = Max(cubes[2], 14)
             select r * g * b;
 
         return possible.Sum().ToString();
     }
 
-    private static int Min(IEnumerable<Cubes> sets, int max) =>
+    private static int Max(IEnumerable<Cubes> sets, int max) =>
         sets.Select(x => x.Count).Max();
 
     private static List<(Game game, List<List<Cubes>> cubes)> FlattenCubes(IEnumerable<Game> games) =>
         games.Select(game => (
-            game,
-            game.Sets
-            .SelectMany(set => set.Cubes)
-            .ToList()
-            .OrderBy(x => x.Color)
-            .GroupBy(x => x.Color)
-            .Select(xs => xs.ToList())
-            .ToList()))
+                game,
+                game.Sets
+                    .SelectMany(set => set.Cubes)
+                    .ToList()
+                    .OrderBy(x => x.Color)
+                    .GroupBy(x => x.Color)
+                    .Select(xs => xs.ToList())
+                    .ToList()))
             .ToList();
 
     private static List<Game> Parse(string input) =>
-        String("Game ")
-            .Then(DecimalNum)
-            .Before(String(": "))
-            .Bind(index => (
-                     DecimalNum
-                    .Before(String(" "))
-                    .Bind(count =>
-                         CIEnum<Color>()
-                        .Map(color => new Cubes(color, count)))
-                    .Separated(String(", ")))
-                .Separated(String("; "))
-                .Map(sets => new Game(
-                    index,
-                    sets.Select(cubes => new Set(cubes.ToList()))
-                        .ToList())))
-            .Separated(String("\n"))
-            .ParseOrThrow(input)
-            .ToList();
+         String("Game ")
+        .Then(DecimalNum)
+        .Before(String(": "))
+        .Bind(index => (
+                 DecimalNum
+                .Before(String(" "))
+                .Bind(count =>
+                     CIEnum<Color>()
+                    .Map(color => new Cubes(color, count)))
+                .Separated(String(", ")))
+            .Separated(String("; "))
+            .Map(sets => new Game(
+                index,
+                sets.Select(cubes => new Set(cubes.ToList()))
+                    .ToList())))
+        .Separated(String("\n"))
+        .ParseOrThrow(input)
+        .ToList();
 
     private readonly record struct Game(int Index, List<Set> Sets);
 
