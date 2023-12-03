c2f525b18f8a7130e095295f4623515275d0d2ba
thinker227 <thinker0227@gmail.com>
2023-12-03 09:47:48 +0100

    Day 3 part 2

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace AdventOfCode;
 
 public sealed class Day3 : Day
@@ -8,10 +10,75 @@ public sealed class Day3 : Day
         var yOffset = lineLength + 3;
         var text = Input.Replace("\n", "...");
 
+        var clusters = GetClusters(text);
+        var adjacent = new HashSet<Cluster>();
+
+        for (var i = 0 ; i < text.Length; i++)
+        {
+            if (text[i] is '.' or >= '0' and <= '9') continue;
+
+            var cs = GetAdjacentClusters(clusters, lineLength, i);
+            foreach (var c in cs) adjacent.Add(c);
+        }
+
+        return adjacent
+            .Select(x => x.Num)
+            .Sum()
+            .ToString();
+    }
+
+    public override string Part2()
+    {
+        var lineLength = Input.IndexOf('\n');
+        var yOffset = lineLength + 3;
+        var text = Input.Replace("\n", "...");
+
+        var clusters = GetClusters(text);
+
+        var result =
+            from x in text.Index()
+            where x.item == '*'
+            let location = x.index
+            let adjacent = GetAdjacentClusters(clusters, lineLength, location)
+            where adjacent.Count == 2
+            select adjacent.Aggregate(1, (x, c) => x * c.Num);
+
+        return result
+            .Sum()
+            .ToString();
+    }
+
+    private static HashSet<Cluster> GetAdjacentClusters(
+        Dictionary<int, Cluster> clusters,
+        int lineLength,
+        int location)
+    {
+        var yOffset = lineLength + 3;
+        var cs = new HashSet<Cluster>();
+
+        void Add(int offset)
+        {
+            if (clusters.TryGetValue(location + offset, out var c)) cs!.Add(c);
+        }
+
+        Add(-yOffset - 1);
+        Add(-yOffset    );
+        Add(-yOffset + 1);
+        Add(         - 1);
+        Add(         + 1);
+        Add(+yOffset - 1);
+        Add(+yOffset    );
+        Add(+yOffset + 1);
+
+        return cs;
+    }
+
+    private static Dictionary<int, Cluster> GetClusters(string text)
+    {
         var n = null as int?;
         var l = 0;
 
-        var clusterLocations = new Dictionary<int, Cluster>();
+        var clusters = new Dictionary<int, Cluster>();
 
         for (var i = 0; i < text.Length; i++)
         {
@@ -27,41 +94,14 @@ public sealed class Day3 : Day
             else if (n is {} b)
             {
                 for (var lx = 0; lx < l; lx++)
-                    clusterLocations[i - 1 - lx] = new(i - l, b);
+                    clusters[i - 1 - lx] = new(i - l, b);
 
                 n = null;
                 l = 0;
             }
         }
 
-        var clusters = new HashSet<Cluster>();
-
-        void AddCluster(int index)
-        {
-            if (clusterLocations.TryGetValue(index, out var cluster))
-                clusters.Add(cluster);
-        }
-
-        for (var i = 0 ; i < text.Length; i++)
-        {
-            var c = text[i];
-
-            if (char.IsAsciiDigit(c) || c == '.') continue;
-
-            AddCluster(i - yOffset - 1);
-            AddCluster(i - yOffset    );
-            AddCluster(i - yOffset + 1);
-            AddCluster(i           - 1);
-            AddCluster(i           + 1);
-            AddCluster(i + yOffset - 1);
-            AddCluster(i + yOffset    );
-            AddCluster(i + yOffset + 1);
-        }
-
-        return clusters
-            .Select(x => x.Num)
-            .Sum()
-            .ToString();
+        return clusters;
     }
 
     private readonly record struct Cluster(int Start, int Num);
