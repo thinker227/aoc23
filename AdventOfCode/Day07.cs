e10438f4d8d23e983e753bc055f66f35f7513b0b
thinker227 <thinker0227@gmail.com>
2023-12-07 12:05:49 +0100

    Day 7 part 2

--- AdventOfCode/Day07.cs
+++ AdventOfCode/Day07.cs
@@ -3,12 +3,26 @@ namespace AdventOfCode;
 public sealed class Day7 : Day
 {
     public override string Part1() =>
+        Solve(HandType, hand => HandOrder(hand, false));
+
+    public override string Part2() =>
+        Solve(
+            hand => HandType(hand.Replace(
+                'J',
+                hand.Where(x => x != 'J')
+                    .GroupBy(x => x)
+                    .MaxBy(x => x.Count())?
+                    .Key
+                    ?? 'J')),
+            hand => HandOrder(hand, true));
+
+    private string Solve(Func<string, int> handType, Func<string, long> handOrder) =>
         Input
             .Split('\n')
             .Select(x => x.Split(' '))
             .Select(xs => (hand: xs[0], bet: int.Parse(xs[1])))
-            .OrderBy(x => HandType(x.hand))
-            .ThenBy(x => HandOrder(x.hand))
+            .OrderBy(x => handType(x.hand))
+            .ThenBy(x => handOrder(x.hand))
             .Index()
             .Select(x => x.item.bet * (x.index + 1))
             .Sum()
@@ -29,16 +43,16 @@ public sealed class Day7 : Day
             _ => 0
         };
 
-    private static long HandOrder(string hand) =>
+    private static long HandOrder(string hand, bool j) =>
         hand.Aggregate(0, (acc, x) =>
-            acc * 100 + CardOrder(x));
+            acc * 100 + CardOrder(x, j));
 
-    private static int CardOrder(char c) => c switch
+    private static int CardOrder(char c, bool j) => c switch
     {
         'A' => 12,
         'K' => 11,
         'Q' => 10,
-        'J' => 9,
+        'J' => j ? -1 : 9,
         'T' => 8,
         _ => c - '2',
     };
