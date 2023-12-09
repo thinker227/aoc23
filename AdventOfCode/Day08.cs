acd4ee6a84e830d66c27299f95ce0686cbf36bef
thinker227 <thinker0227@gmail.com>
2023-12-09 07:35:00 +0100

    Non-functioning day 8 part 2

--- AdventOfCode/Day08.cs
+++ AdventOfCode/Day08.cs
@@ -4,7 +4,8 @@ public sealed class Day8 : Day
 {
     public override string Part1()
     {
-        var lines = Input.Split('\n');
+        var lines = Input
+            .Split('\n');
 
         var nodes = lines[2..]
             .ToDictionary(
@@ -23,5 +24,56 @@ public sealed class Day8 : Day
             .index;
 
         return x.ToString();
+    }
+
+    // Doesn't work
+    public override string Part2()
+    {
+        var lines = Input.Split('\n');
+
+        var seq = lines[0];
+
+        var nodes = lines[2..]
+            .ToDictionary(
+                x => x[0..3],
+                x => new Node(
+                    x[0..3],
+                    x[7..10],
+                    x[12..15]));
+
+        List<Node> FindLoop(Node start)
+        {
+            var visited = new HashSet<(Node, int)>();
+            var path = new List<Node>();
+
+            Visit(start, 0);
+
+            void Visit(Node node, int pos)
+            {
+                if (visited.Contains((node, pos))) return;
+                visited.Add((node, pos));
+
+                var p = seq![pos];
+                var t = p == 'L' ? node.Left : node.Right;
+                var n = nodes![t];
+                path.Add(node);
+
+                Visit(n, (pos + 1) % seq.Length);
+            }
+
+            return path;
+        }
+
+        foreach (var node in nodes.Values.Where(x => x.Name.EndsWith('A')))
+        {
+            var loop = FindLoop(node);
+        }
+
+        throw new NotImplementedException();
+    }
+
+    private readonly record struct Node(string Name, string Left, string Right)
+    {
+        public override string ToString() => $"{Name} = ({Left}, {Right})";
     }
 }
