f69f52e49765cc7cd309521547b0736aa72a4ea0
thinker227 <thinker0227@gmail.com>
2023-12-14 11:30:57 +0100

    Day 14 part 1

---
+++ AdventOfCode/Day14.cs
@@ -0,0 +1,26 @@
+namespace AdventOfCode;
+
+public sealed class Day14 : Day
+{
+    public override string Part1()
+    {
+        var size = Input.IndexOf('\n');
+
+        return Input
+            .Split('\n')
+            .ZipMany()
+            .Select(xs => xs
+                .Index()
+                .Split(x => x.item == '#')
+                .Where(g => g.Any())
+                .Select(g => (
+                    start: Input.IndexOf('\n') - g.First().index,
+                    count: g.Count(x => x.item == 'O')))
+                .Select(g => Enumerable
+                    .Range(g.start - g.count + 1, g.count)
+                    .Sum())
+                .Sum())
+            .Sum()
+            .ToString();
+    }
+}
